2cb979a572b334d12bdf1f03bb961f5e7b51798b
LuckyHappyFish <colinwong0224@gmail.com>
2024-11-17 23:47:33 -0500

    Initial commit

---
+++ src/test/java/com/example/diabetes_predictor/DiabetesPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diabetes_predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
